ade893438c6f2d21620591c9280a2d685c084f16
MJEngeloch <mariejo.engeloch@gmail.com>
2019-07-24 15:17:07 +0200

    Creat a template and a home files

---
+++ src/view/frontend/home.php
@@ -0,0 +1,12 @@
+<?php $title = Accueil ?>
+
+<?php ob_start(); ?>
+
+<h1>Hello World</h1>
+
+<?php
+phpinfo(); ?>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
---
+++ src/view/frontend/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content=""> <!-- A compléter -->
+    <link href="https://fonts.googleapis.com/css?family=Advent+Pro:400,500|Raleway:400,400i,700,700i&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?= $title ?></title>
+    <link href="public/css/style.scss" rel="stylesheet" />
+    <!--Ajouter les icon-->
+</head>
+
+<body>
+    <?= $content ?>
+</body>
+
+</html>

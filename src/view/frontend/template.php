0b7f79b73ed330853662df1fe7884e57769d5661
MJEngeloch <mariejo.engeloch@gmail.com>
2019-07-29 15:24:52 +0200

    Setup "Header"

--- src/view/frontend/template.php
+++ src/view/frontend/template.php
@@ -18,31 +18,35 @@ require "vendor/autoload.php";
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-md fixed-top navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src="public/images/Logo.png" id="logo" alt="Logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav class="navbar navbar-expand-md sticky-top navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="public/images/Logo.png" id="logo" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">A propos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                    </ul>
-                </div>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
+    <header>
+        <div id="title">
+            <h1>Un billet simple pour l'Alaska</h1>
+            <p id="subtitle">De Jean Forteroche</p>
+        </div>
     </header>
     <?= $content ?>
     <footer>

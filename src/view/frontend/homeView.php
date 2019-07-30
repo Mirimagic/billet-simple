7e82bd51af7cbec4688c105fb4b322df783653c5
MJEngeloch <mariejo.engeloch@gmail.com>
2019-07-30 10:07:39 +0200

    Setup CSS for section Last Chapter

--- src/view/frontend/homeView.php
+++ src/view/frontend/homeView.php
@@ -9,25 +9,25 @@
                 <h2>Dernier chapitre</h2>
             </div>
         </div>
-        <div class="row" id="display-last-chapter">
+        <div id="display-last-chapter">
             <div class="row">
-                <div class="col-5 offset-col-1">
+                <div class="offset-1">
                     <p id="last-chapter-numbre">Chapitre X</p>
                 </div>
             </div>
             <div class="row">
-                <div class="col-5 offset-col-1">
+                <div class="offset-1">
                     <h3>Titre du chapitre</h3>
                 </div>
             </div>
             <div class="row">
-                <div class="col-5 offset-col-1">
+                <div class="col-5 offset-1">
                     <button>Lire le chapitre</button>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 date">
                 <p>Sorti le DATE – X Commentaire(s)</p>
             </div>
         </div>

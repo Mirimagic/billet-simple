e88f996bc6fffa416437f3f7e366b1bec28b73c9
MJEngeloch <mariejo.engeloch@gmail.com>
2019-08-05 09:53:30 +0200

    Setup database

--- src/index.php
+++ src/index.php
@@ -29,7 +29,7 @@ try {
 
 /* phpinfo(); */
 
-$link = mysqli_connect("172.17.0.2:3306", "root", "example", "Test");
+$link = mysqli_connect("db", "root", "example", "Test");
 
 if (!$link) {
     echo "Erreur : Impossible de se connecter à MySQL." . PHP_EOL;
